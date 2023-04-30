e2ee8bfcb7eda9bc4712b223c724b1ad1c5fe672
joshua ramos <joshuaramos@nyu.edu>
2023-04-29 20:37:15 -0400

    Completed Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+// function () {
+//     destructivelyAppendCat(name)
+//   }
+  
+//   function cats() {
+//     ["Milo", "Otis", "Garfield"]
+//     }
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+
+function appendCat(name) { 
+    return [...cats, name];
+}
+function prependCat(name) {
+    console.log(name)
+    return [name, ...cats];
+}
+function removeLastCat() {
+    return (cats.slice(0, cats.length - 1));
+}
+function removeFirstCat() {
+    return (cats.slice(1));
+}
